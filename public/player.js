e53ead57c9f515c68b91cd97da879b5d386797e3
tiyanzxbellas <tiyabbella@gmail.com>
2026-06-28 15:26:59 +0700

    Update player.js

--- public/player.js
+++ public/player.js
@@ -12,9 +12,9 @@ function cn(t){if(!t)return'Unknown';return t.replace(/[^\x20-\x7E\xA0-\xFF\u010
 function gid(id){return document.getElementById(id);}
 
 function updateOG(title,image){
-    var t=document.querySelector('meta[property="og:title"]');if(!t){t=document.createElement('meta');t.setAttribute('property','og:title');document.head.appendChild(t);}t.setAttribute('content',title+' | NanzMusify');
+    var t=document.querySelector('meta[property="og:title"]');if(!t){t=document.createElement('meta');t.setAttribute('property','og:title');document.head.appendChild(t);}t.setAttribute('content',title+' | Musicall');
     var i=document.querySelector('meta[property="og:image"]');if(!i){i=document.createElement('meta');i.setAttribute('property','og:image');document.head.appendChild(i);}i.setAttribute('content',image||FI);
-    document.title=title+' - NanzMusify';
+    document.title=title+' - Musicall';
 }
 
 const yt=document.createElement('script');yt.src="https://www.youtube.com/iframe_api";document.head.appendChild(yt);
